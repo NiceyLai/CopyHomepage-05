7bf381a533d299dd3ff7a0a7ea7c1b7cac8f90ed
nicey <1919325005@qq.com>
2022-08-04 10:56:48 +0800

    swiper插件使用

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+  var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+  });
+});
